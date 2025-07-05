db00a89010153885bac76aeda8b70289b23939f8
Leonardo <lifetutors.lc@gmail.com>
2025-07-05 20:17:56 -0300

    feat: initilize project

---
+++ src/main/java/com/lithoykai/radio_peao/RadioPeaoApplication.java
@@ -0,0 +1,13 @@
+package com.lithoykai.radio_peao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadioPeaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadioPeaoApplication.class, args);
+	}
+
+}
