f35b5871b6497e5e6ccba77055658da6dd9d2ade
Leonardo <lifetutors.lc@gmail.com>
2025-07-05 21:28:16 -0300

    feat: websocket added

---
+++ src/main/java/com/lithoykai/radio_peao/RadioState.java
@@ -0,0 +1,12 @@
+package com.lithoykai.radio_peao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RadioState {
+    private List<String> queue = new ArrayList<>();
+    private String currentUrl;
+    private double currentTime;
+    private boolean isPlaying;
+}
+
---
+++ src/main/java/com/lithoykai/radio_peao/controller/SocketController.java
@@ -0,0 +1,18 @@
+package com.lithoykai.radio_peao.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import java.util.Map;
+
+@Controller
+public class SocketController {
+
+    @MessageMapping("/control")
+    @SendTo("/topic/control")
+    public Map<String, Object> handleControl(Map<String, Object> message) {
+        System.out.println("[WebSocket] Mensagem recebida: " + message);
+        return message;
+    }
+}
